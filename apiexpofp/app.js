8eba1b75f4d7eeefd70bd756efa6df2846f2388a
CesarMrRiver <cesar.mitical@gmail.com>
2018-01-23 13:06:25 +0100

    añadido el resto de dependencias y recursos para sus funcionalidades

--- apiexpofp/app.js
+++ apiexpofp/app.js
@@ -2,11 +2,24 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const logger =  require('morgan'); // Para realizar el logging de la aplicación
+const mongoose = require('mongoose');
+const auth = require('./middlewares/auth');
+const config = require('./config');
+
+
+/*
+mongoose.connect(config.MONGODB_URI,
+    { useMongoClient: true });
+*/
+mongoose.Promise = global.Promise; //para la creacion de peticiones
+
 
 const users = require('./routes/users');
 
 let app = express();
 
+app.use(logger('dev')); // Para "inicializar" el logging
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
