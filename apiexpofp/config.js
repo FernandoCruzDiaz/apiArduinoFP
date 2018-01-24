35201930ac3eba2d31a8972177259892747c2bf9
franciscojosetis <franciscojosetis@gmail.com>
2018-01-24 13:50:41 +0100

    Subida registro de usuarios

--- apiexpofp/config.js
+++ apiexpofp/config.js
@@ -1,11 +1,4 @@
-
 module.exports = {
-<<<<<<< HEAD
-    SECRET_TOKEN: '',
+    SECRET_TOKEN: 'mititotieneunatienda',
     MONGODB_URI: 'mongodb://expofp:expofp@ds213688.mlab.com:13688/cuentaexpofp_2018'
 };
-=======
-    SECRET_TOKEN: 'miTitoTieneUnaTiendaYoTengoUnPimporritoQueEchaAguaFresquita',
-    MONGODB_URI: 'mongodb://expofp:expofp@ds213688.mlab.com:13688/cuentaexpofp_2018'
-}
->>>>>>> 781f6330075ae99b6e2d171680b44ec14dcfdc5f
