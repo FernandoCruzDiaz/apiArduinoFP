0b36f7399e2bba3f1359a095416d0f3c69ea6f45
franciscojosetis <franciscojosetis@gmail.com>
2018-01-29 12:41:33 +0100

    Quitar el init automatico

--- apiexpofp/app.js
+++ apiexpofp/app.js
@@ -11,45 +11,7 @@ const placas = require('./routes/placa');
 const consume = require('./routes/consume');
 
 mongoose.connect(config.MONGODB_URI,
-    { useMongoClient: true }).then(() => {
-
-    const Placa = require('./models/placa');
-    const Consume = require('./models/consume');
-    /*
-    Placa.find((err, result) => {
-        if (!(result && result.length)) {
-            let placa = new Placa({
-                user: mongoose.Types.ObjectId('5a6880bccdec7417a421a199'),
-                id_placa: '1'});
-            placa.save((err, result) => {
-                if (err) return console.error(`Error de inserción: ${err.message}`);
-                console.log(`Insertado: ${result}`)
-            });
-        } else{
-            console.log('Ya hay Datos');
-        }
-    });*/
-
-    Consume.find((err, result) => {
-        if(!(result && result.length)){
-            let consume = new Consume({
-               id_placa: mongoose.Types.ObjectId('5a6edba28bdf80231cbee9ac'),
-               consumo: 34,
-               fecha_Inicio: Date.now(),
-               fecha_Fin: Date.now()
-            });
-            consume.save((err, result) => {
-                if (err) return console.error(`Error de inserción: ${err.message}`);
-                console.log(`Insertado: ${result}`)
-            });
-        } else{
-            console.log('Ya hay Datos');
-        }
-    });
-
-}, (err) => {
-    console.error(`Error de conexión: ${err.message}`);
-});
+    { useMongoClient: true });
 
 mongoose.Promise = global.Promise; //para la creacion de peticiones
 
